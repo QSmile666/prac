f41e4f8a9b05a2876edb6b0624dfc5dc1a6474a6
lizhenhua@bonree.com <lizhenhua@bonree.com>
2021-06-29 20:32:10 +0800

    0629练习

--- MultiEnv/env_demo.py
+++ MultiEnv/env_demo.py
@@ -3,7 +3,7 @@ import requests
 
 class API:
     env = {
-        "default": "test3",
+        "default": "test2",
         "devtest": {
             "test2": "20317",
             "test3": "20392",
@@ -11,7 +11,7 @@ class API:
     }
 
     def send(self, data: dict):
-        data["url"] = str(data["url"]).replace("devtest", self.env["devtest"][self.env["default"]])
+        data["url"] = str(data["url"]).replace("20392", self.env["devtest"][self.env["default"]])
         r = requests.request(method=data["method"], url=data["url"], json=data["payload"],
                              headers=data["headers"])
         return r
