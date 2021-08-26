16fefa14a72cdfac8490d7038933e4dbc0228816
zhenhualee <zhenhua_hh@163.com>
2021-08-26 20:37:25 +0800

    实现部门的增删改查。

--- test_wework/test_wework_api.py
+++ test_wework/test_wework_api.py
@@ -13,7 +13,7 @@ class TestWeworkApi:
         assert res.json()["errcode"] == 0
         return self.token
 
-    def test_wework_api(self):
+    def test_member_api(self):
         # 删除成员
         userid = "zhangsan"
         url = f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={self.token}&userid={userid}"
@@ -23,7 +23,7 @@ class TestWeworkApi:
 
         # 创建成员
         url = f"https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={self.token}"
-        create_data = {
+        create_memberData = {
             "userid": "zhangsan",
             "name": "张三",
             "alias": "jackzhang",
@@ -31,7 +31,7 @@ class TestWeworkApi:
             "email": "zhangsan@gzdev.com",
         }
 
-        res = requests.post(url=url, json=create_data)
+        res = requests.post(url=url, json=create_memberData)
         print(res.json())
         assert res.json()["errcode"] == 0
 
@@ -44,10 +44,51 @@ class TestWeworkApi:
 
         # 更新成员
         url = f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={self.token}"
-        update_data = {
+        update_memberData = {
             "userid": userid,
             "name": "张三",
         }
-        res = requests.post(url=url, json=update_data)
+        res = requests.post(url=url, json=update_memberData)
+        print(res.json())
+        assert res.json()["errcode"] == 0
+
+    def test_department_api(self):
+        # 删除部门
+        departId = 10
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/department/delete?access_token={self.token}&id={departId}"
+        res = requests.get(url=url)
+        print(res.json())
+        assert res.json()["errcode"] == 0
+
+        # 创建部门
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/department/create?access_token={self.token}"
+        create_departData = {
+            "name": "yy研发中心",
+            "name_en": "RDGZ",
+            "parentid": 1,
+            "order": 1,
+            "id": 10
+        }
+        res = requests.post(url=url, json=create_departData)
+        print(res.json())
+        assert res.json()["errcode"] == 0
+
+        # 查看部门
+        departId = 10
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token={self.token}&id={departId}"
+        res = requests.get(url=url)
+        print(res.json())
+        assert res.json()["errcode"] == 0
+
+        # 修改部门
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/department/update?access_token={self.token}"
+        update_departData = {
+            "id": departId,
+            "name": "yys研发中心",
+            "name_en": "yysRDGZ",
+            "parentid": 1,
+            "order": 1
+        }
+        res = requests.post(url=url, json=update_departData)
         print(res.json())
         assert res.json()["errcode"] == 0
