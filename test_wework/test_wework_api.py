0031b299e42303bcc7b996526642af87a6be0831
zhenhualee <zhenhua_hh@163.com>
2021-08-26 20:21:13 +0800

    提取token在类中最开始运行一次，用于其他函数的使用。实现通讯录成员的增删改查。

--- test_wework/test_wework_api.py
+++ test_wework/test_wework_api.py
@@ -2,18 +2,28 @@ import requests
 
 
 class TestWeworkApi:
-    def test_wework_api(self):
-        corpid = "ww6c619306416691d9"
-        corpsecret = "2nWlaax4KBCNZZ9N0GNw1LhkpZx3qfBcKLKMlCj02O8"
-        url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}"
+    corpid = "ww6c619306416691d9"
+    corpsecret = "2nWlaax4KBCNZZ9N0GNw1LhkpZx3qfBcKLKMlCj02O8"
+
+    def setup(self):
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={self.corpid}&corpsecret={self.corpsecret}"
         res = requests.get(url=url)
         print(res.json())
-        token = res.json()["access_token"]
+        self.token = res.json()["access_token"]
         assert res.json()["errcode"] == 0
+        return self.token
+
+    def test_wework_api(self):
+        # 删除成员
+        userid = "zhangsan"
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={self.token}&userid={userid}"
+        res = requests.get(url=url)
+        print(res.json())
+        # assert res.json()["errcode"] == 0
 
         # 创建成员
-        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={token}"
-        body = {
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={self.token}"
+        create_data = {
             "userid": "zhangsan",
             "name": "张三",
             "alias": "jackzhang",
@@ -21,23 +31,23 @@ class TestWeworkApi:
             "email": "zhangsan@gzdev.com",
         }
 
-        res = requests.post(url=url, json=body)
+        res = requests.post(url=url, json=create_data)
         print(res.json())
         assert res.json()["errcode"] == 0
 
         # 查看成员
-        userid = "zhangsan"
-        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={token}&userid={userid}"
+        # userid = "zhangsan"
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={self.token}&userid={userid}"
         res = requests.get(url=url)
         print(res.json())
         assert res.json()["errcode"] == 0
 
-        # 删除成员
-        userid = "zhangsan"
-        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={token}&userid={userid}"
-        res = requests.get(url=url)
+        # 更新成员
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={self.token}"
+        update_data = {
+            "userid": userid,
+            "name": "张三",
+        }
+        res = requests.post(url=url, json=update_data)
         print(res.json())
         assert res.json()["errcode"] == 0
-
-    def test_getMembers(self):
-        pass
